e442947c59bf910bb6ac17c39b40172312b9f7b9
Vani20-0 <sanjeebani2018@gmail.com>
2025-11-01 02:46:27 +0545

    navbar and rules

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Steps from "./pages/Steps";
+import Rules from "./pages/Rules";
 
 export default function App() {
   return( 
     <Router>
       <Routes>
-        <Route path="/steps" element={<Steps />} /> {/* temporary */}
+        <Route path="/steps" element={<Steps />} />
+      </Routes>
+
+      <Routes>
+        <Route path="/rules" element={<Rules />} />
       </Routes>
     </Router>
   );
--- src/pages/Steps.jsx
+++ src/pages/Steps.jsx
@@ -1,12 +1,20 @@
 import ElectionStepsSection  from "../components/ElectionSteps.jsx";
 import Footer from "../components/Footer.jsx";
 import Header from "../components/Header.jsx";
+import Navigation from "../components/Navbar.jsx";
 import { BookOpen, CheckCircle, Users } from "lucide-react";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white w-full" style={{ margin: 0, padding: 0 }}>
-  <Header/>
+  <div className="bg-white border-b border-gray-200">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between py-4">
+            <Header />
+            <Navigation />
+          </div>
+        </div>
+      </div>
 
   {/* Hero Section - Full width */}
   <div class="bg-gradient-to-b from-white to-blue-50 py-12 px-4 text-center border-b border-gray-100">
