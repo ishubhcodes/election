86b702eed9e48910d5d229e088a1155422086947
ishubhcodes <ydvshubham123@gmail.com>
2025-11-01 03:18:33 +0545

    vote info page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import VoteDemo from "./pages/VoteDemo";
+import VoteInfo from "./pages/VoteInfo";
 import { Toaster } from "sonner";
+
 
 export default function App() {
   return( 
     <>
     <Toaster position='top-right' richColors toastOptions={{ className: 'text-xl p-6 w-96 h-16' }} />
-    <VoteDemo/>
+    <VoteInfo/>
     </>
   );
 }
